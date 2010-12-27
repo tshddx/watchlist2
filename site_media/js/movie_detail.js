063eb52176db34ee16d36874d68f7a1f96e4bb79
Thomas Shaddox <wediddoit@gmail.com>
2010-12-26 23:35:42 -0600

    Styled header elements. Styled the comments section on movie_detail.

--- site_media/js/movie_detail.js
+++ site_media/js/movie_detail.js
@@ -1,7 +1,7 @@
 $(document).ready(function() 
     { 
         form = $("div.comments-form");
-        link = $("a.show-comments-form");
+        link = $("p.show-comments-form");
         form.hide();
         link.show();
         link.click(function(e) {
