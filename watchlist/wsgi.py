ec195e3edcc3a4ef469cb97cdf1c38fd15567ddb
Thomas Shaddox <wediddoit@gmail.com>
2012-04-21 23:52:53 -0500

    heroku blah

--- watchlist/wsgi.py
+++ watchlist/wsgi.py
@@ -1,11 +1,28 @@
-#!/usr/bin/python
-import sys, os
+"""
+WSGI config for hellodjango project.
 
-sys.path.append(os.path.dirname(__file__))
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
-if "DJANGO_SETTINGS_MODULE" not in os.environ:
-    os.environ['DJANGO_SETTINGS_MODULE'] = "settings"
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
 
-import django.core.handlers.wsgi
+"""
+import os
 
-application = django.core.handlers.wsgi.WSGIHandler()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "watchlist2.settings")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
